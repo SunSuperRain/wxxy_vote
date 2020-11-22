3376e993dbc516746b3c658c510c58992dd39efa
zhangdoudou-end <2532423306@qq.com>
2020-11-22 16:43:21 +0800

    后台文件的位置

--- src/main/java/com/sun/controller/pageController.java
+++ src/main/java/com/sun/controller/pageController.java
@@ -3,7 +3,7 @@ package com.sun.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/page")
+@Controller
 public class pageController {
 
     @RequestMapping("/page/index")
@@ -11,4 +11,43 @@ public class pageController {
         return "back/index";
     }
 
+    @RequestMapping("/home/console")
+    public String layuiConsole(){
+        return "back/home/console";
+    }
+
+    @RequestMapping("/template/addresslist")
+    public String layuiAdd(){
+        return "back/template/addresslist";
+    }
+
+    @RequestMapping("/administrators/list-person")
+    public String layuiListPerson(){
+        return "back/user/administrators/list-person";
+    }
+
+    @RequestMapping("/administrators/list")
+    public String layuiList(){
+        return "back/user/administrators/list";
+    }
+
+    @RequestMapping("/administrators/role")
+    public String layuiRole(){
+        return "back/user/administrators/role";
+    }
+
+    @RequestMapping("/set/user/info")
+    public String layuiInfo(){
+        return "back/set/user/info";
+    }
+
+    @RequestMapping("/set/user/password")
+    public String layuiPass(){
+        return "back/set/user/password";
+    }
+
+    @RequestMapping("/timu")
+    public String layuiTimu(){
+        return "back/user/user/timu";
+    }
 }
